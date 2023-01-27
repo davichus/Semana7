d6df9a856e1765354a5975dca62d3f56f73e6098
davidnavarrete <davidnava1998efrain@outlook.com>
2023-01-26 19:36:26 -0500

    version final

---
+++ DnavarreteS7/DnavarreteS7/DnavarreteS7/ConsultaRegistro.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using SQLite;
+using System.Collections.ObjectModel;
+
+namespace DnavarreteS7
+{
+    public partial class ConsultaRegistro : ContentPage
+    {
+        private SQLiteAsyncConnection con;
+        private ObservableCollection<Estudiante> tEstudiante;
+
+
+        public ConsultaRegistro()
+        {
+            InitializeComponent();
+            con = DependencyService.Get<DataBase>().GetConnection();
+            Listar();
+        }
+
+        public async void Listar()
+        {
+            var resultado = await con.Table<Estudiante>().ToListAsync();
+            tEstudiante = new ObservableCollection<Estudiante>(resultado);
+            ListaEstudiantes.ItemsSource = tEstudiante;
+        }
+
+        //void OnSelection(System.Object sender, SelectedItemChangedEventArgs e)
+        //{
+         
+        //}
+
+        void btnRegresar_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Login());
+        }
+
+        void ListaEstudiantes_ItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            var obj = (Estudiante)e.Item;
+            var item = obj.Id.ToString();
+            var Id = Convert.ToInt32(item);
+            var Nombre = obj.Nombre.ToString();
+            var Usuario = obj.Usuario.ToString();
+            var Contrasena = obj.Contrasenia.ToString();
+            Navigation.PushAsync(new Elemento(Id, Nombre, Usuario, Contrasena));
+        }
+    }
+}
+
---
+++ DnavarreteS7/DnavarreteS7/DnavarreteS7/Elemento.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using SQLite;
+using System.IO;
+
+namespace DnavarreteS7
+{
+    public partial class Elemento : ContentPage
+    {
+        public int IdSeleccionado;
+        private SQLiteAsyncConnection con;
+        IEnumerable<Estudiante> rEliminar;
+        IEnumerable<Estudiante> rActualizar;
+
+        public Elemento(int id, string Nombre, string Usuario, string Contrasena)
+        {
+            InitializeComponent();
+            txtNombre.Text = Nombre;
+            txtUsuario.Text = Usuario;
+            txtContrasena.Text = Contrasena;
+            IdSeleccionado = id;
+
+        }
+
+        public static IEnumerable<Estudiante> Delete(SQLiteConnection db, int id)
+        {
+            return db.Query<Estudiante>("DELETE FROM Estudiante WHERE Id =?", id);
+        }
+
+
+        public static IEnumerable<Estudiante> Update(SQLiteConnection db, int id, string nombre, string usuario, string contrasena)
+        {
+            return db.Query<Estudiante>("UPDATE Estudiante SET Nombre = ?, Usuario = ?, Contrasenia = ? WHERE Id =?", nombre, usuario, contrasena, id);
+        }
+
+        void btnActualizar_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db = new SQLiteConnection(ruta);
+                rEliminar = Update(db, IdSeleccionado, txtNombre.Text, txtUsuario.Text, txtContrasena.Text);
+                Navigation.PushAsync(new ConsultaRegistro());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+        }
+
+        void btnEliminar_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db = new SQLiteConnection(ruta);
+                rEliminar = Delete(db, IdSeleccionado);
+                Navigation.PushAsync(new ConsultaRegistro());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+        }
+    }
+}
+
